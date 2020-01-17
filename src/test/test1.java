dd40c8116c37b1c6ac1957d077de53589c0866a3
tkdwl <tkdwls9277>
2020-01-17 09:46:46 +0900

    first commit

---
+++ src/test/test1.java
@@ -0,0 +1,32 @@
+package test;
+
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String args[]) {
+    	int a;
+    	int mok=0, ext;
+    	int sum=0;
+    	int count=0;
+    	Scanner s = new Scanner(System.in);
+    	a = s.nextInt();
+    	
+    	for(int i=1;i<=a;i++) {
+    		mok=i;
+    		sum=0;
+    		while(true) {
+    			
+    			if(mok==0)
+    				break;
+    			ext=mok%10;
+    			mok=mok/10;
+    			
+    			sum=sum+ext;
+    		}
+    		
+    		if(i%sum==0)
+    			count++;
+    	}
+    	System.out.println(count);
+    }
+}
