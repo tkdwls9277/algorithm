751989cd280f4ee8f9285ccf8bb0ba797dd2a751
tkdwl <tkdwls9277>
2020-01-20 12:21:38 +0900

    브루트포스로 구현한 블랙잭

--- src/test/Algorithm2798.java
+++ src/test/Algorithm2798.java
@@ -1,10 +1,34 @@
+//블랙잭(브루트포스)
+
 package test;
 
+import java.util.Scanner;
 public class Algorithm2798 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int count, M, memo=0;
+		int[] N;
+		
+		Scanner s = new Scanner(System.in);
+		
+		count = s.nextInt();
+		M=s.nextInt();
+		N=new int[count];
+		for(int i=0;i<N.length;i++) {
+			N[i]=s.nextInt();
+		}
+		for(int j=0;j<N.length;j++) {
+			for(int k=j+1;k<N.length;k++) {
+				for(int h=k+1;h<N.length;h++) {
+					if(N[j]+N[k]+N[h]<=M && N[j]+N[k]+N[h]>=memo) {
+						
+						memo=N[j]+N[k]+N[h];
+					}
+				}
+			}
+		}
+		System.out.println(memo);
 	}
 
 }
