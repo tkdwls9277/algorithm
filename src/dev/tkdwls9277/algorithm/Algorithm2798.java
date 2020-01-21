9a86fbbc58ed2dc7946c3c64d9a1439bbb4d1d98
danielk0121 <maedk10@gmail.com>
2020-01-21 15:29:11 +0900

    주석 추가

--- src/dev/tkdwls9277/algorithm/Algorithm2798.java
+++ src/dev/tkdwls9277/algorithm/Algorithm2798.java
@@ -1,16 +1,18 @@
-//블랙잭(브루트포스)
-
 package dev.tkdwls9277.algorithm;
 
 import java.util.Scanner;
+
+/**
+ * 블랙잭(브루트포스) <=== 클래스 주석 위치 이동
+ */
 public class Algorithm2798 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int count, M, memo=0;
+		// TODO Auto-generated method stub  // FIXME <=== 의미 없는 자동완성 주석 삭제
+		int count, M, memo=0;  // FIXME 변수명 M 의미있는 변수명으로 바꿔주세요
 		int[] N;
 		
-		Scanner s = new Scanner(System.in);
+		Scanner s = new Scanner(System.in);  // FIXME 변수명 s 무의미, stdin 시스템 리소스인데 close 안해서 warnning 뜹니다
 		
 		count = s.nextInt();
 		M=s.nextInt();
